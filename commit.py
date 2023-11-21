37ef83b2a80bd92c6c1869cf4a9152b8bf7ad343
Hydro83 <mirens@protonmail.com>
2023-11-21 04:16:33 +0100

    Initial commit

--- commit.py
+++
@@ -1,9 +0,0 @@
-import os
-
-input("Commit changes to github? Press ENTER")
-
-os.system('git add --all')
-os.system('git commit -m "Initial commit"')
-os.system('git push -u origin main')
-
-input("Press Enter to exit...")
